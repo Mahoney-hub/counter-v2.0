a85c4bb113b3833c9037d69454278bd876bfe177
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-05 17:20:23 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Box, Button, ButtonGroup, Paper, TextField, Typography} from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // BLL
+    const [count, setCount] = useState<number>(0)
+    const [error, setError] = useState<boolean>(false)
+    // Functions
+    const increaseValue = () => {
+        if (!error) setCount(count + 1)
+    }
+    const resetValue = () => {
+        if (count !== 0) setCount(0)
+    }
 
-export default App;
+    return (
+        <Box className={'container'}>
+            <Paper elevation={24} className={'block'}>
+                <Box m={2} className={'row'}>
+                    <Typography variant={'h6'}>max value:</Typography>
+                    <TextField
+                        label="Number"
+                        type="number"
+                        size={'small'}
+                        color={'primary'}
+                        focused
+                        InputLabelProps={{
+                            // shrink: true,
+                        }}
+                    />
+                </Box>
+                <Box m={2} className={'row'}>
+                    <Typography variant={'h6'}>set value:</Typography>
+                    <TextField
+                        label="Number"
+                        type="number"
+                        size={'small'}
+                        color={'primary'}
+                        focused
+                        InputLabelProps={{
+                            // shrink: true,
+                        }}
+                    />
+                </Box>
+                <ButtonGroup color={'primary'} variant="contained" size="large" fullWidth>
+                    <Button>set</Button>
+                </ButtonGroup>
+            </Paper>
+            <Paper elevation={24} className={'block'}>
+                <Box className={'counter'}>
+                    <Typography color={(error) ? 'red' : 'black'} variant={'h1'}>{count}</Typography>
+                </Box>
+                <ButtonGroup color={'primary'} variant="contained" size="large" fullWidth>
+                    <Button onClick={increaseValue} disabled={error}>increase</Button>
+                    <Button onClick={resetValue} disabled={(count === 0)}>reset</Button>
+                </ButtonGroup>
+            </Paper>
+        </Box>
+    );
+};
+
+export default App
